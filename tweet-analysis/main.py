a620f4e1c4265cc976b381a1255f34da674f577f
EbenOcloo <119362310+EbenOcloo@users.noreply.github.com>
2022-11-28 21:49:31 +0000

    Add files via upload

---
+++ tweet-analysis/main.py
@@ -0,0 +1,60 @@
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+import cleantext
+
+
+st.header('Bluecrest Sentiment Analyzer')
+with st.expander('Analyze Text'):
+    text = st.text_input('Text here: ')
+    if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+
+
+    pre = st.text_input('Clean Text: ')
+    if pre:
+        st.write(cleantext.clean(pre,
+                                lower=True ,no_numbers=True , no_punct=True))
+
+with st.expander('Analyze CSV'):
+    upl = st.file_uploader('Upload file')
+
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
+
+#
+    def analyze(x):
+        if x >= 0.5:
+            return 'Positive'
+        elif x <= -0.5:
+            return 'Negative'
+        else:
+            return 'Neutral'
+
+#
+    if upl:
+        df = pd.read_excel(upl)
+        del df['Unnamed: 0']
+        df['score'] = df['tweets'].apply(score)
+        df['analysis'] = df['score'].apply(analyze)
+        st.write(df.head(100))
+
+        @st.cache
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+        csv = convert_df(df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='sentiment.csv',
+            mime='text/csv',
+        )
+
+
+
